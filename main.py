131c76cd3fbdb50d94469fecd0c1cea8a8d43b09
Colman Swisher <cswisher@acistl.com>
2018-04-22 13:31:48 -0500

    Blogz with everything but the current signed-in validation

--- main.py
+++ main.py
@@ -6,15 +6,15 @@ endpoints_without_login = ['login', 'signup']
 
 #Handlers------------------------------------------------------------
 
-@app.before_request
+#@app.before_request
 def require_login():
     if not ('user' in session or request.endpoint in endpoints_without_login):
         return redirect("/login")
 
-@app.route("/logout", methods=['POST'])
+@app.route("/logout", methods=['POST', 'GET'])
 def logout():
     del session['user']
-    return redirect("/blog")
+    return redirect("/login")
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
@@ -24,16 +24,21 @@ def login():
         email = request.form['email']
         password = request.form['password']
         users = User.query.filter_by(email=email)
+        if (password.strip() == "") and (email.strip() == ""):
+            flash("Please login.")
+            return redirect("/login")
         if users.count() == 1:
             user = users.first()
             if password == user.password:
                 session['user'] = user.email
                 flash('welcome back, '+user.email)
                 return redirect("/newpost")
+            if password != user.password:
+                flash('Incorrect password.')
+                return render_template("login.html", email=email)
+        else:
             flash('User does not exist.')
             return redirect("/signup")
-        flash('Incorrect password.')
-        return redirect("/login")
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
@@ -42,7 +47,7 @@ def signup():
         password = request.form['password']
         verify = request.form['verify']
         if not is_email(email):
-            flash('zoiks! "' + email + '" does not seem like an email address')
+            flash('zoiks! ' + email + ' does not seem like an email address')
             return redirect('/signup')
         email_db_count = User.query.filter_by(email=email).count()
         if email_db_count > 0:
@@ -50,27 +55,39 @@ def signup():
             return redirect('/signup')
         if password != verify:
             flash('passwords did not match')
-            return redirect('/signup')
-        user = User(email=email, password=password)
+            return render_template("signup.html", email=email)
+        user = User(email, password)
         db.session.add(user)
         db.session.commit()
         session['user'] = user.email
+        flash("Welcome to the blog," + email + "!")
         return redirect("/")
-    else:
-        return render_template('signup.html')
+    
+    return render_template('signup.html')
 
-def is_email(string):
-    # for our purposes, an email string has an '@' followed by a '.'
-    # there is an embedded language called 'regular expression' that would crunch this implementation down
-    # to a one-liner, but we'll keep it simple:
-    atsign_index = string.find('@')
-    atsign_present = atsign_index >= 0
-    if not atsign_present:
+def is_email(email):
+    count_at = 0
+    count_period = 0
+    count_space = 0
+    count_email = 0
+    for i in email:
+        count_email = count_email + 1
+        if i == "@":
+            count_at = count_at + 1
+        else:
+            count_at = count_at
+        if i == ".":
+            count_period = count_period + 1
+        else:
+            count_period = count_period
+        if i == " ":
+            count_space = count_space + 1
+        else:
+            count_space = count_space
+    if (count_period < 1) or (count_at != 1) or (count_space > 0) or (count_email > 30 or count_email < 3):
         return False
     else:
-        domain_dot_index = string.find('.', atsign_index)
-        domain_dot_present = domain_dot_index >= 0
-        return domain_dot_present
+        return True
 
 @app.route('/blog', methods=['POST', 'GET'])
 def read():
@@ -87,8 +104,10 @@ def index():
 
 @app.route('/singleUser', methods=['GET', 'POST'])
 def MyBlogs():
-    blogposts = Blog.query.filter_by(owner_id=session['user']).first()
-    return render_template('blog.html', blogposts=blogposts)
+    
+    owner = User.query.filter_by(email=session['user']).first()
+    blogposts = Blog.query.filter_by(owner=owner).all()
+    return render_template('singleUser.html', blogposts=blogposts)
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def newpost():
@@ -96,13 +115,11 @@ def newpost():
     if request.method == 'POST':
         new_title = request.form['title']
         new_content = request.form['content']
-        title_error = ""
-        content_error = ""
         if (not new_title) or (new_title.strip() == ""):
-            title_error = "Please add a title to your blog post."
+            flash("Please add a title to your blogpost.")
         if (not new_content) or (new_content.strip() == ""):
-            content_error = "Please add some content to your blog post."
-        if (title_error == "") and (content_error == ""):
+            flash("Please add content to your blogpost.")
+        elif (new_title or new_title.strip() != "") and (new_content or new_content.strip() != ""):
             owner = User.query.filter_by(email=session['user']).first()
             new_blog = Blog(new_title, new_content, owner)
             db.session.add(new_blog)
@@ -111,10 +128,8 @@ def newpost():
             return redirect(link)
             #render_template('blogpost.html', title=new_title, body=new_content)
             #redirect('./blog?id={0}', id=new_blog.id, title=new_title, body=new_content)
-        else:
-            return render_template('newpost.html', 
-            content_error=content_error, title_error=title_error, 
-            new_content=new_content, new_title=new_title)
+        return render_template('newpost.html', 
+        new_content=new_content, new_title=new_title)
         
     else:
         return render_template('newpost.html')
